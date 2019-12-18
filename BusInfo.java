248008a4242daede3d41e35333b06637ec740c33
ByeongSeop <46362397+ByeongSeop@users.noreply.github.com>
2019-12-18 14:12:29 +0900

    Update BusInfo.java

--- BusInfo.java
+++ BusInfo.java
@@ -3,6 +3,7 @@ package com.byeongseop.nt900.daeguebusapp;
 
 public class BusInfo {
   int res_id;
-  String route_no,arr_state, cur_pos;//버스 번호, 남은시간,현재위치
+  String routeNo, arrState, curPos;//버스 번호, 남은시간,현재위치
+  boolean isLowBus; //저상버스이면 true 아니면 false
 
 }
