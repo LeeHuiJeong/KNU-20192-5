156e0380104a67625f2af67b97eab4a0ad0411e6
ByeongSeop <46362397+ByeongSeop@users.noreply.github.com>
2019-12-18 14:14:29 +0900

    Update BusInfoView.java

--- BusInfoView.java
+++ BusInfoView.java
@@ -1,14 +1,12 @@
 package com.byeongseop.nt900.daeguebusapp;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class BusInfoView extends LinearLayout {
-    LinearLayout image;
+    TextView bus_image;
     TextView route_no, arr_state, cur_pos;
     BusInfo data;
 
@@ -22,11 +20,13 @@ public class BusInfoView extends LinearLayout {
         route_no=(TextView)findViewById(R.id.route_no);
         arr_state=(TextView)findViewById(R.id.arr_state);
         cur_pos=(TextView)findViewById(R.id.cur_pos);
-        image=(LinearLayout)findViewById(R.id.busImage);
+        bus_image=(TextView)findViewById(R.id.busImage);
 
-        route_no.setText(data.route_no);
-        arr_state.setText(data.arr_state);
-        cur_pos.setText(data.cur_pos);
-        //image.setBackgroundResource(data.res_id);//배경 사진 설정
+        route_no.setText(data.routeNo);
+        arr_state.setText(data.arrState);
+        cur_pos.setText(data.curPos);
+
+        if(data.isLowBus==true)
+            bus_image.setText("저상\nBus");//저상버스일 경우 이미지 변경하기
     }
 }
